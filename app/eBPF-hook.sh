0582796189b5ccbc1c6bbebaffae7b7a201cfe3b
AstixxxX <fake.me100500@gmail.com>
2026-06-08 01:58:43 +0300

    Update eBPF-hook.sh

--- app/eBPF-hook.sh
+++ app/eBPF-hook.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-bpftrace -e '
-tracepoint:syscalls:sys_enter_execve,
-tracepoint:syscalls:sys_enter_connect,
-{
-    printf("[!!!] MALICIOUS BEHAVIOR DETECTED ---> Possible RCE\n");
-    printf("%s(%d): %s\n", comm, pid, probe);
-}'
+
